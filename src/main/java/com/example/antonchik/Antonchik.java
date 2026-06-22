d5bbb95dde09dd245b0de63ef73f03b075e49ca5
Alexey <skyketip@gmail.com>
2026-06-22 23:40:09 +0200

    init

---
+++ src/main/java/com/example/antonchik/Antonchik.java
@@ -0,0 +1,31 @@
+package com.example.antonchik;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Antonchik.MODID, name = Antonchik.NAME, version = Antonchik.VERSION)
+public class Antonchik
+{
+    public static final String MODID = "antonchik";
+    public static final String NAME = "Antonchik";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
